0f6c9ca3a1318b14509a3db4089d543b9f91b76f
Matt Hughes <matt.hughes@arterys.com>
2018-03-06 10:43:02 -0700

    Add name as required by chef 12.0 and up
    [otf: 4605]

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license          "Apache 2.0"
 description      "Installs/Configures zeromq"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.8"
+name             "zeromq"
